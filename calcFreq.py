5a833a7b2ebcd4b5be29ff0062154598053d368d
[Ahmed Ghumman] <[ghumman@vt.edu]>
2016-12-29 15:04:44 +0000

    Comments are added

--- calcFreq.py
+++ calcFreq.py
@@ -1,16 +1,28 @@
-import RPi.GPIO as GPIO
-import time
-from time import sleep
+import RPi.GPIO as GPIO #GPIO pins for raspberry pi 
+import time 		# for using functions like time	
+from time import sleep	# for giving some delay
 
+# don't show me warnings like GPIO pin is already being used by other processes, just overwrite it. 
 GPIO.setwarnings(False)
+
+# We'll be using GPIO BCM notation for pinouts of raspberry pi
 GPIO.setmode(GPIO.BCM)
+
+# Set Pin 25 to take input from signal whose frequency we need to measure
+# Set 24 to output
 GPIO.setup(25, GPIO.IN)
 GPIO.setup(24, GPIO.OUT)
 
-
+# Initialization of variables. We need to have finalTime so that we can use this value for initial frequency calculation
 finalTime = time.time() 
 frequency = 0
+GPIO.output(24, 0)
+lowFreq = 0
 
+
+# callback funciton: occur whenever we get a rising edge interrupt
+# frequency = 1 / duration
+# duratation = time of current rising edge - time of previous rising edge
 def my_callback(channel):
 	if GPIO.input(25):
 		global finalTime
@@ -20,19 +32,16 @@ def my_callback(channel):
 		frequency = 1 / duration
 		print frequency
 		finalTime = time.time() 
-	
+
+# call funciton-> my_callback whenever we get rising edge at pin 25	
 GPIO.add_event_detect(25, GPIO.RISING, callback=my_callback, bouncetime=80)
 
-GPIO.output(24, 0)
-lowFreq = 0
-
+# if frequency is less than 4 increment lowFreq
+# if lowFreq is increment to 4 set the pin 24 
 try: 
 	while True:
 	
-		print "Frequency before if function"
-		print frequency
 		if frequency < 4: 
-			print "Inside frequency < 4"
 			lowFreq+=1
 			if lowFreq > 4:
 				print "Frequency has gone below 4"
@@ -40,7 +49,8 @@ try:
 		sleep(1)
 
 
+# release every gpio pin 
 finally:
-	GPIO.clearnup()
+	GPIO.cleanup()
 
 
